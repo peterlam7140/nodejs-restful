5c2d76ab6b22a7d496a5df11df4c6a5db278ea30
CS Lam <>
2025-04-04 01:28:18 +0800

    modify mongo config and gitignore

--- models/config.js
+++ models/config.js
@@ -1,10 +1,10 @@
 config = {}
 
 config.systemName = 'Student Score Management System';
-config.mongoUrl = 'mongodb+srv://stud1:1234@cluster0.6pcfvh7.mongodb.net/?retryWrites=true&w=majority'; 
+config.mongoUrl = 'mongodb+srv://stud1:1234@cluster0.fqp2y.mongodb.net/?retryWrites=true&w=majority'; 
 config.dbName = 'miniProject';
 config.studentCollectionName = 'students';
-config.mongooseUrl = 'mongodb+srv://stud1:1234@cluster0.6pcfvh7.mongodb.net/miniProject?retryWrites=true&w=majority';
+config.mongooseUrl = 'mongodb+srv://stud1:1234@cluster0.fqp2y.mongodb.net/miniProject?retryWrites=true&w=majority';
 config.SECRETKEY = 'zxsgR3LkJsyZKxpfy2Ju';
 
 module.exports = config;
